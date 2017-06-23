cc72b86fae0d6c49092207ee6ae4d1e5f2fb7183
Jailson Dias <jcd2@cin.ufpe.br>
2017-06-23 17:33:03 -0300

    first commit

---
+++ src/Main.hs
@@ -0,0 +1,29 @@
+module Main where
+
+-- main :: IO ()
+-- main = putStrLn "Hello, Haskell!"
+    import SFML.Window
+
+
+    main = do
+        desktopMode <- getDesktopMode
+        fsModes <- getFullscreenModes
+        
+        putStrLn $ "Current desktop mode:\n\n" ++ show desktopMode
+        putStrLn ""
+        putStrLn $ "Fullscreen modes:"
+        putStrLn ""
+        mapM_ (\m -> putStrLn (show m) >> putStrLn "") fsModes
+        
+        let ctxSettings = Just $ ContextSettings 24 8 0 1 2 [ContextDefault]
+        wnd <- createWindow (VideoMode 640 480 32) "SFML Haskell Demo" [SFDefaultStyle] ctxSettings
+        loop wnd
+        destroy wnd
+
+
+    loop :: Window -> IO ()
+    loop wnd = do
+        evt <- waitEvent wnd
+        case evt of
+            Just SFEvtClosed -> return ()
+            _ -> loop wnd
