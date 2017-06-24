9bcac184a578b1bd582af0d2a8485e9a5273f4c3
Jailson Dias <jcd2@cin.ufpe.br>
2017-06-24 20:50:56 -0300

    quadrados

--- src/Mapa.hs
+++ src/Mapa.hs
@@ -22,89 +22,106 @@ module Mapa where
         cor 1 1 1
         ] 
 
-    for :: Vertice -> Vertice -> Vertice -> Vertice -> [Cor] -> IO ()
-    for v1 v2 v3 v4 [cor] = cube cor v1 v2 v3 v4
-    for v1 v2 v3 v4 (cor:ls) = do 
-        cube cor v1 v2 v3 v4
-        for (mais v1 0.1) (mais v2 0.1) (mais v3 0.1) (mais v4 0.1) ls
+
+    -- Desenha uma linha do mapa
+    linha :: Vertice -> Vertice -> Vertice -> Vertice -> [Cor] -> IO ()
+    linha v1 v2 v3 v4 [cor] = desenhaQuadrado cor v1 v2 v3 v4
+    linha v1 v2 v3 v4 (cor:ls) = do 
+        desenhaQuadrado cor v1 v2 v3 v4
+        linha (mais v1 0.1) (mais v2 0.1) (mais v3 0.1) (mais v4 0.1) ls
             where mais = (\(vx,vy) x -> (vx + x, vy))
 
-    desenhaLinha :: Vertice -> Vertice -> Vertice -> Vertice -> Int -> IO ()
-    desenhaLinha v1 v2 v3 v4 1 = for v1 v2 v3 v4 [
-                (cor 1 0 0),
-                (cor 1 0 0),
-                (cor 1 0 0),
-                (cor 1 0 0),
-                (cor 1 0 0),
-                (cor 1 0 0),
-                (cor 1 0 0),
-                (cor 1 0 1),
-                (cor 1 1 0),
-                (cor 1 1 0),
-                (cor 1 0 0),
-                (cor 1 0 0),
-                (cor 1 0 1),
-                (cor 1 0 0),
-                (cor 1 0 0),
-                (cor 1 0 0),
-                (cor 1 0 0),
-                (cor 1 0 0),
-                (cor 1 0 0),
-                (cor 1 0 0)
-                ]
-    desenhaLinha v1 v2 v3 v4 a = do 
-        for v1 v2 v3 v4 [
-                (cor 1 0 0),
-                (cor 1 0 0),
-                (cor 1 0 0),
-                (cor 1 0 0),
-                (cor 1 0 0),
-                (cor 1 0 0),
-                (cor 1 0 0),
-                (cor 1 0 1),
-                (cor 1 1 0),
-                (cor 1 1 0),
-                (cor 1 0 0),
-                (cor 1 0 0),
-                (cor 1 0 1),
-                (cor 1 0 0),
-                (cor 1 0 0),
-                (cor 1 0 0),
-                (cor 1 0 0),
-                (cor 1 0 0),
-                (cor 1 0 0),
-                (cor 1 0 0)
-                ]
-        desenhaLinha (menos v1 0.1) (menos v2 0.1) (menos v3 0.1) (menos v4 0.1) (a-1)
+    -- Chama a função para desenhar cada linha do mapa
+    desenhaMapa :: Vertice -> Vertice -> Vertice -> Vertice -> [[Cor]] -> IO ()
+    desenhaMapa v1 v2 v3 v4 [cor] = linha v1 v2 v3 v4 cor
+    desenhaMapa v1 v2 v3 v4 (cor:ls) = do 
+        linha v1 v2 v3 v4 cor
+        -- linha v1 v2 v3 v4 [
+        --         (cor 1 0 0),
+        --         (cor 1 0 0),
+        --         (cor 1 0 0),
+        --         (cor 1 0 0),
+        --         (cor 1 0 0),
+        --         (cor 1 0 0),
+        --         (cor 1 0 0),
+        --         (cor 1 0 1),
+        --         (cor 1 1 0),
+        --         (cor 1 1 0),
+        --         (cor 1 0 0),
+        --         (cor 1 0 0),
+        --         (cor 1 0 1),
+        --         (cor 1 0 0),
+        --         (cor 1 0 0),
+        --         (cor 1 0 0),
+        --         (cor 1 0 0),
+        --         (cor 1 0 0),
+        --         (cor 1 0 0),
+        --         (cor 1 0 0)
+        --         ]
+        desenhaMapa (menos v1 0.1) (menos v2 0.1) (menos v3 0.1) (menos v4 0.1) ls
             where menos = (\(vx,vy) y -> (vx, vy - y))
+    
+    getCores:: [[Int]] -> [[Cor]]
+    getCores [] = []
+    getCores (linha:ls) = [ cores!!c | c <- linha ] : getCores ls
 
-    gerarCor :: Int -> [Int]
-    gerarCor 0 = []
-    gerarCor q = do 
-        -- cd <- randomRIO (0::Int, 4::Int)
-        let cd = unsafePerformIO (randomRIO (0::Int, 4::Int))
-        -- putStrLn (show cd)
-        cd : gerarCor (q-1)
+    
+    -- Gerar uma lista de numeros entre 0 e 4
+    randomLinha :: Int -> IO([Int])
+    randomLinha 0 = return []
+    randomLinha n = do
+        r  <- randomRIO (0,4)
+        rs <- randomLinha (n-1)
+        return (r:rs) 
 
+    randomMapa :: Int -> Int -> IO [[Int]]
+    randomMapa 0 l = return []
+    randomMapa n l = do
+        linha <- randomLinha l
+        mapa <- randomMapa (n-1) l
+        return (linha:mapa) 
+
+
+    -- Desenha o mapa inicial do jogo
     mapa :: IO ()
     mapa = do
-        cd <- randomRIO (1::Int, 10::Int)
-        putStrLn (show cd)
+        -- cd <- take 400 randomRIO (0::Int, 4::Int)
+        cd <- randomMapa 20 20
+        -- putStrLn (show cd)
         clear [ColorBuffer]
         renderPrimitive Quads $ do
-            desenhaLinha (vertice (-1) 1) (vertice (-1) 0.9) (vertice (-0.9) 0.9) (vertice (-0.9) 1) 20
+            desenhaMapa (vertice (-1) 1) (vertice (-1) 0.9) (vertice (-0.9) 0.9) (vertice (-0.9) 1) (getCores cd)
         flush
 
+    -- Utilizado para chamar color3f em vez de chamar color $ Color3 r g b
     color3f :: Cor -> IO ()
     color3f (r, g, b) = color $ Color3 r g (b :: GLfloat)
     
+    -- Utilizado para chamar vertex2f em vez de chamar vertex $ Vertex2 x y
     vertex2f :: Vertice -> IO ()
     vertex2f (x, y) = vertex $ Vertex2 x y
 
-    cube :: Cor -> Vertice -> Vertice -> Vertice -> Vertice -> IO ()
-    cube cor v1 v2 v3 v4 = do
+
+    -- Desenha apenas um quadrado
+    desenhaQuadrado :: Cor -> Vertice -> Vertice -> Vertice -> Vertice -> IO ()
+    desenhaQuadrado cor v1 v2 v3 v4 = do
             color3f cor
             vertex2f v1
             vertex2f v2
             vertex2f v3
             vertex2f v4
+
+
+
+
+
+
+    -- Gerar lista de cores (Provisorio)
+    corLinha :: Int -> [Cor]
+    corLinha 1 = cor 1 0 0 : []
+    corLinha q = cor 0 0 1 : corLinha (q-1)
+
+    corMapa :: Int -> Int -> [[Cor]]
+    corMapa 1 l = corLinha l : []
+    corMapa q l = corLinha l : corMapa (q-1) l
+
